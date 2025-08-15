062687e453d3785d3847bf0a856d87303f40deee
Amit Saha <asaha.bwn2000@gmail.com>
2025-08-16 01:17:56 +0530

    setup axios config

--- src/util/axiosConfig.jsx
+++ src/util/axiosConfig.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
+import { BASE_URL } from "./apiEndpoints";
 
 const axiosConfig = axios.create({
-  baseURL: "https://bucksbee.onrender.com/api/v1.0",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
 
+// these endpoints does not require auth header
 const excludeEndpoints = [
   "/login",
   "/register",
@@ -15,3 +17,43 @@ const excludeEndpoints = [
   "/activate",
   "/status",
 ];
+
+// request interceptor
+axiosConfig.interceptors.request.use(
+  (config) => {
+    const shouldSkipToken = excludeEndpoints.some((endpoint) => {
+      config.url?.includes(endpoint);
+    });
+
+    if (!shouldSkipToken) {
+      const accessToken = localStorage.getItem("token");
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// response interceptor
+axiosConfig.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.error("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. Please try again.");
+    }
+
+    return Promise.reject(error);
+  }
+);
